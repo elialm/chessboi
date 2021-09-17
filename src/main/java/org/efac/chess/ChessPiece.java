3ab57089cd41904c01368cf2e21d4784e16863a3
Elijah Almeida Coimbra <ealmeidacoimbra@gmail.com>
2021-09-17 17:28:04 +0200

    Added BoardLocation bounds check

--- src/main/java/org/efac/chess/ChessPiece.java
+++ src/main/java/org/efac/chess/ChessPiece.java
@@ -1,14 +1,33 @@
 package org.efac.chess;
 
 import org.efac.chess.BoardLocation;
+import org.efac.chess.Chessboard;
 
 public abstract class ChessPiece {
     BoardLocation location;
+    Chessboard board;
     
     public BoardLocation getLocation() { return location; }
+    public Chessboard getChessboard() { return board; }
 
-    public ChessPiece(BoardLocation location) {
+    public ChessPiece(BoardLocation location, Chessboard board) {
+        if (location == null) {
+            throw new NullPointerException("location cannot be null");
+        }
+
+        if (board == null) {
+            throw new NullPointerException("board cannot be null");
+        }
+
+        board.checkIfInBounds(location);
+        
         this.location = location;
+        this.board = board;
+    }
+
+    public ChessPiece(int xLocation, int yLocation, Chessboard board) {
+        this.location = board.createLocation(xLocation, yLocation);
+        this.board = board;
     }
 
     public abstract BoardLocation[] getPossibleMoves();
--- src/main/java/org/efac/chess/piece/Bishop.java
+++ src/main/java/org/efac/chess/piece/Bishop.java
@@ -1,11 +1,16 @@
 package org.efac.chess.piece;
 
-import org.efac.chess.BoardLocation;
 import org.efac.chess.ChessPiece;
+import org.efac.chess.BoardLocation;
+import org.efac.chess.Chessboard;
 
 public class Bishop extends ChessPiece {
-    public Bishop(BoardLocation location) {
-        super(location);
+    public Bishop(BoardLocation location, Chessboard board) {
+        super(location, board);
+    }
+
+    public Bishop(int xLocation, int yLocation, Chessboard board) {
+        super(xLocation, yLocation, board);
     }
 
     public BoardLocation[] getPossibleMoves() {
