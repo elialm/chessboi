8b5b82556d22e01222e6b0581dd7acd18bd37e92
Elijah Almeida Coimbra <ealmeidacoimbra@gmail.com>
2021-09-17 18:18:07 +0200

    Added reference to Chessboard to location

--- src/main/java/org/efac/chess/BoardLocation.java
+++ src/main/java/org/efac/chess/BoardLocation.java
@@ -1,26 +1,53 @@
 package org.efac.chess;
 
-import java.security.InvalidParameterException;
-
 import org.efac.chess.Chessboard;
+import org.efac.chess.ChessPiece;
+import java.security.InvalidParameterException;
 
 public class BoardLocation {
     int xLocation;
     int yLocation;
+    Chessboard chessboard;
     
     public int getXLocation() { return xLocation; }
     public int getYLocation() { return yLocation; }
+    public Chessboard getChessboard() { return chessboard; }
 
-    public BoardLocation(int x, int y, Chessboard chessboard) {
-        if (x >= chessboard.getXSize()) {
-            throw new InvalidParameterException("x must be smaller then chessboard's x size");
-        }
-
-        if (y >= chessboard.getYSize()) {
-            throw new InvalidParameterException("y must be smaller then chessboard's y size");
+    public BoardLocation(int xLocation, int yLocation, Chessboard chessboard) {
+        if (chessboard == null) {
+            throw new NullPointerException("chessboard cannot be null");
         }
         
-        xLocation = x;
-        yLocation = y;
+        if (xLocation >= chessboard.getXSize()) {
+            throw new InvalidParameterException("xLocation must be smaller then chessboard's x size");
+        }
+
+        if (yLocation >= chessboard.getYSize()) {
+            throw new InvalidParameterException("yLocation must be smaller then chessboard's y size");
+        }
+
+        if (xLocation < 0) {
+            throw new InvalidParameterException("xLocation must be positive");
+        }
+
+        if (yLocation < 0) {
+            throw new InvalidParameterException("yLocation must be positive");
+        }
+        
+        this.xLocation = xLocation;
+        this.yLocation = yLocation;
+        this.chessboard = chessboard;
+    }
+
+    public ChessPiece getPiece() {
+        return chessboard.getPiece(xLocation, yLocation);
+    }
+
+    public void setPiece(ChessPiece piece) {
+        chessboard.setPiece(xLocation, yLocation, piece);
+    }
+
+    public BoardLocation getRelativeLocation(int xRelative, int yRelative) {
+        return chessboard.createLocation(xLocation + xRelative, yLocation + yRelative);
     }
 }
