fcbfafb6a671c312bdd73072c5e6fd2e4dbbe76d
Elijah Almeida Coimbra <ealmeidacoimbra@gmail.com>
2021-09-18 23:17:23 +0200

    Working event handling

---
+++ src/main/java/org/efac/ChessboardController.java
@@ -0,0 +1,12 @@
+package org.efac;
+
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+
+public class ChessboardController {
+    
+    @FXML
+    public void pressed(ActionEvent event) {
+        System.out.println("pressed!");
+    }
+}
