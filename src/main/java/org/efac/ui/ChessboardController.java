80c85d45c78b9deb799f946d29e587d6add84638
Elijah Almeida Coimbra <ealmeidacoimbra@gmail.com>
2021-09-19 02:06:07 +0200

    Made ui more user friendly

--- src/main/java/org/efac/ui/ChessboardController.java
+++ src/main/java/org/efac/ui/ChessboardController.java
@@ -1,9 +1,38 @@
 package org.efac.ui;
 
+/**
+ *  MIT License
+
+    Copyright (c) 2021 Elijah Almeida Coimbra
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
+ */
+
 import javafx.fxml.FXML;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventType;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
@@ -30,9 +59,18 @@ public class ChessboardController {
     @FXML
     private GridPane chessboardPane;
 
+    @FXML
+    private Button chessboardUpdate;
+
     public ChessboardController() {
         numberFormatExceptionPattern = Pattern.compile("For input string: \"(.*)\"");
         chessboard = null;
+    }
+
+    @FXML
+    private void initialize() {
+        addTextFieldFocusedListener(chessboardWidth);
+        addTextFieldFocusedListener(chessboardHeight);
     }
 
     @FXML
@@ -63,6 +101,21 @@ public class ChessboardController {
         ((Button)event.getSource()).setText("Update dimensions");
     }
 
+    @FXML
+    public void handleChessboardWidthOnKeyPressed(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER) {
+            chessboardHeight.requestFocus();
+        }
+    }
+
+    @FXML
+    public void handleChessboardHeightOnKeyPressed(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER) {
+            chessboardPane.requestFocus();
+            chessboardUpdate.fire();
+        }
+    }
+
     private void handleInvalidIntegerInput(NumberFormatException ex) {
         Matcher matcher = numberFormatExceptionPattern.matcher(ex.getMessage());
         matcher.matches();
@@ -84,5 +137,13 @@ public class ChessboardController {
 
         this.chessboardWidth.setText("8");
         this.chessboardHeight.setText("8");
+    }
+
+    private void addTextFieldFocusedListener(TextField textField) {
+        textField.focusedProperty().addListener((obs, wasFocused, isNowFocused) -> {
+            Platform.runLater(() -> {
+                textField.selectAll();
+            });
+        });
     }
 }
